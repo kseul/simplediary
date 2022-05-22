2e79558038d2e98454f3b34372cf8f049eebf3c2
seul <kseul06@gmail.com>
2022-05-22 10:52:01 +0900

    Add : 데이터 수정하기

--- src/App.js
+++ src/App.js
@@ -20,7 +20,20 @@ function App() {
     setData([newItem, ...data]); // 원래 데이터에다가 newItem을 이어붙이는 효과 -> 반대순서면 newItem이 먼저
   };
 
-  const onDelete = (targetId) => {
+  // 특정 일기 데이터를 수정하는 함수
+  const onEdit = (targetId, newContent) => {
+    console.log(targetId);
+    console.log(newContent);
+    setData(
+      // 원본 데이터 배열에 map으로 순회, 새로운 수정된 배열 만들어서 반환
+      // 수정대상이라면 객체 수정, 아니라면 그대로 it 객체
+      data.map((it) =>
+        it.id === targetId ? { ...it, content: newContent } : it
+      )
+    );
+  };
+
+  const onRemove = (targetId) => {
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
@@ -28,7 +41,7 @@ function App() {
   return (
     <div className='App'>
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} onDelete={onDelete} />
+      <DiaryList diaryList={data} onEdit={onEdit} onRemove={onRemove} />
     </div>
   );
 }

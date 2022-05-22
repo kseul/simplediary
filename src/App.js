37da10f9c5872dc547166071ec1ae953c1e2d641
seul <kseul06@gmail.com>
2022-05-22 10:15:21 +0900

    Add : 리스트 삭제하기  filter()

--- src/App.js
+++ src/App.js
@@ -3,32 +3,8 @@ import './App.css';
 import DiaryList from './Diary.List';
 import DiaryEditor from './DiaryEditor';
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: '슬비',
-//     content: '하이1',
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 2,
-//     author: '오예',
-//     content: '하이2',
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-//   {
-//     id: 3,
-//     author: '꼬비',
-//     content: '하이3',
-//     emotion: 4,
-//     created_date: new Date().getTime(),
-//   },
-// ];
-
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([]); // 일기가 없는 상태 빈배열
   const dataId = useRef(0);
 
   const onCreate = (author, content, emotion) => {
@@ -44,10 +20,15 @@ function App() {
     setData([newItem, ...data]); // 원래 데이터에다가 newItem을 이어붙이는 효과 -> 반대순서면 newItem이 먼저
   };
 
+  const onDelete = (targetId) => {
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    setData(newDiaryList);
+  };
+
   return (
     <div className='App'>
       <DiaryEditor onCreate={onCreate} />
-      <DiaryList diaryList={data} />
+      <DiaryList diaryList={data} onDelete={onDelete} />
     </div>
   );
 }
--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
-const DiaryItem = ({ author, content, emotion, created_date }) => {
+const DiaryItem = ({
+  author,
+  content,
+  emotion,
+  created_date,
+  id,
+  onDelete,
+}) => {
   return (
     <div className='DiaryItem'>
       <div className='info'>
@@ -11,6 +18,15 @@ const DiaryItem = ({ author, content, emotion, created_date }) => {
         <span className='date'>{new Date(created_date).toLocaleString()}</span>
       </div>
       <div className='content'>{content}</div>
+      <button
+        onClick={() => {
+          if (window.confirm(`${id}번째 일기를 정말 삭제할건가요?`)) {
+            onDelete(id);
+          }
+        }}
+      >
+        삭제하기
+      </button>
     </div>
   );
 };
